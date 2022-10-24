b0df34dd492523bbac546ea4fec7151c86923f29
GlobalSwap Exchange <111717023+globalswapx@users.noreply.github.com>
2022-10-24 23:08:22 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+#  GlobalSwap UIkit
+
+GlobalSwap UIkit is a set of React components and hooks used to build pages on Globalswap's apps. It also contains a theme file for dark and light mode.
+
+## Install
+
+`yarn add @globalswap-libs/uikit`
+
+## Setup
+
+### Theme
+
+Before using GlobalSwap UIkit, you need to provide the theme file to styled-component.
+
+```
+import { ThemeProvider } from 'styled-components'
+import { light, dark } from '@globalswap-libs/uikit'
+...
+<ThemeProvider theme={isDark}>...</ThemeProvider>
+```
+
+### Reset
+
+A reset CSS is available as a global styled component.
+
+```
+import { ResetCSS } from '@globalswap-libs/uikit'
+...
+<ResetCSS />
+```
+
+### Types
+
+This project is built with Typescript and export all the relevant types.
+
+## How to use the UIkit
+
+If you want to use components from the UIkit, check the [Storybook documentation](https://GlobalSwap.github.io/globalswap-uikit/)
---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,154 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://dex.globalswap.exchange",
+      },
+      {
+        label: "Liquidity",
+        href: "https://dex.globalswap.exchange/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://globalswap.exchange",
+      },
+      {
+        label: "Tokens",
+        href: "https://globalswap.exchange/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://globalswap.exchange/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://globalswap.exchange/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      // {
+      //   label: "Voting",
+      //   href: "https://voting.pancakeswap.finance",
+      // },
+      {
+        label: "Github",
+        href: "https://github.com/globalswapx/",
+      },
+      {
+        label: "Docs",
+        href: "https://globalswap-exchange.gitbook.io/globalswap/",
+      },
+      {
+        label: "Blog",
+        href: "https://globalswapx.medium.com/",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/globalswapExchange",
+      },
+      // {
+      //   label: "Bahasa Indonesia",
+      //   href: "https://t.me/PancakeSwapIndonesia",
+      // },
+     
+     
+      
+      {
+        label: "Announcements",
+        href: "https://t.me/globalswap_notification",
+      },
+     
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/globalswapex",
+  },
+  {
+    label: "Reddit",
+    icon: "RedditIcon",
+    href: "https://www.reddit.com/r/globalswapex",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
